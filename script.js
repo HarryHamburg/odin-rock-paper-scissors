3e11a22f87c3179304766cb5b91d771b12d67800
Harry Hamburg <119865998+HarryHamburg@users.noreply.github.com>
2025-02-24 09:18:09 +0000

    create and complete the getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello Harry!")
+function getComputerChoice () {
+
+    let pick = Math.floor (Math.random() * 3);
+
+        if (pick === 0) {
+            computerChoice = "Rock";
+        } 
+        
+        else if (pick === 1) {
+            computerChoice = "Paper";
+        }
+        
+        else {
+            computerChoice = "Scissors";
+        }
+    
+    return computerChoice;
+
+}
+
+console.log(getComputerChoice())
