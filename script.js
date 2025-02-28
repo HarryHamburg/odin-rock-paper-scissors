58cfd45bb35a0823dd4a090a52815de22cbacff5
Harry Hamburg <119865998+HarryHamburg@users.noreply.github.com>
2025-02-28 11:36:50 +0000

    finish and optimise existing functions, create and complete playGame function, add Play Game button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getComputerChoice () {
 
     let pick = Math.floor (Math.random() * 3);
-
+let computerChoice;
         if (pick === 0) {
             computerChoice = "rock";
         } 
@@ -20,7 +20,6 @@ function getComputerChoice () {
 
 //console.log(getComputerChoice())
 
-//let humanChoice = prompt("Let's play! Type rock, paper or scissors");
 
 function getHumanChoice () {
 let humanChoice = prompt("Let's play! Type rock, paper or scissors");
@@ -29,7 +28,8 @@ if  (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "sciss
     return humanChoice;
 }
 else {
-    return alert("Unfortunately this game isn't that smart. You can only chose rock paper or scissors. Try again...");
+    alert("Unfortunately this game isn't that smart. You can only chose rock paper or scissors. Try again...");
+    return getHumanChoice();
 }
 
 }
@@ -42,14 +42,65 @@ let computerScore = 0;
 function playRound (human, computer) {
 
 if (human === "rock" && computer === "scissors") {
-    return alert("The computer chose scissors, you win!");
+    humanScore++;
+    return alert("The old decrepit computer chose scissors, you win!");
+}
+else if (human === "rock" && computer === "paper") {
+    computerScore++;
+    return alert("The old decrepit computer chose paper, you lose!");
+}
+else if (human === "rock" && computer === "rock") {
+    alert("You both chose rock! It's a draw.");
 }
 
+else if (human === "paper" && computer === "rock") {
+    humanScore++;
+    return alert("The old decrepit computer chose rock, you win!");
+}
+else if (human === "paper" && computer === "scissors") {
+    computerScore++;
+    return alert("The old decrepit computer chose scissors, you lose!");
+}
+else if (human === "paper" && computer === "paper") {
+    alert("You both chose paper! It's a draw.");
+}
+
+else if (human === "scissors" && computer === "paper") {
+    humanScore++;
+    return alert("The old decrepit computer chose paper, you win!");
+}
+else if (human === "scissors" && computer === "rock") {
+    computerScore++;
+    return alert("The old decrepit computer chose rock, you lose!");
+}
+else if (human === "scissors" && computer === "scissors") {
+    alert("You both chose scissors! It's a draw.");
+}
 
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-console.log(playRound(humanSelection, computerSelection));
+function playGame () {
+alert ("Welcome to rock, paper, scissors. We're going to play 5 rounds. It's you vs the old decrepit computer. Let's go!")
+playRound(getHumanChoice(), getComputerChoice());
+alert (`after round 1/5, the score is, you: ${humanScore}, the old decrepit computer: ${computerScore}. Time for round 2.`)
+playRound(getHumanChoice(), getComputerChoice());
+alert (`after round 2/5, the score is, you: ${humanScore}, the old decrepit computer: ${computerScore}. Time for round 3.`)
+playRound(getHumanChoice(), getComputerChoice());
+alert (`after round 3/5, the score is, you: ${humanScore}, the old decrepit computer: ${computerScore}. Time for round 4.`)
+playRound(getHumanChoice(), getComputerChoice());
+alert (`after round 4/5, the score is, you: ${humanScore}, the old decrepit computer: ${computerScore}. Time for the final round.`)
+playRound(getHumanChoice(), getComputerChoice());
+if (humanScore > computerScore) {
+    alert (`Congratulations, you won the game! You beat the old decrepit computer with a final score of, you: ${humanScore}, the old decrepit computer: ${computerScore}.`)
+}
+else if (humanScore < computerScore) {
+    alert (`Oh no!, you lost the game! The old decrepit computer beat you with a final score of, you: ${humanScore}, the old decrepit computer: ${computerScore}.`)
+}
+else if (humanScore === computerScore) {
+    alert (`The final score is a draw! You won ${humanScore} rounds, and the old decrepit computer also won ${computerScore} rounds.`)
+}
+humanScore = 0;
+computerScore = 0;
+alert ("Thank you for playing. You can use the Play the game button if you want to play again.")
+}
 
